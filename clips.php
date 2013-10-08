6afe8f3e712afddfcb1f97332b43c96b346cb252
parisminton <parisminton@da.ydrea.ms>
2013-10-08 02:18:12 -0400

    Edited portfolio list.

--- clips.php
+++ clips.php
@@ -39,24 +39,8 @@
 
         <div id="portfolio-block">
           <h2 style="margin:50px 0px 10px 0px;">Portfolio</h2>
-          <p class="port-hed"><a href="/pdfs/chartervote_2011_1109.pdf">Detroiters approve revised city charter</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 1A | 11/09/11</p>
-
-          <p class="port-hed"><a href="/pdfs/911_2011_0911.pdf">Steel from World Trade Center towers used for memorials</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 8A | 9/11/11</p>
-          
-          <p class="port-hed"><a href="/pdfs/security_2011_0606.pdf">Palmer Woods residents on board with paramilitary-style patrols</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 1A | 6/6/11</p>
-          
           <p class="port-hed"><a href="/pdfs/redford_2011_0322.pdf">Turnaround in Redford Blueprint for Detroit Works</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 1A | 3/22/11</p>
-
-          <p class="port-hed"><a href="/pdfs/buenosaires_2011_116.pdf">South America, city and country</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 7E | 1/26/11</p>
-
-
-          <p class="port-hed"><a href="/pdfs/terrorist_2009_1226.pdf">Feds: Terror foiled on flight to Detroit</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 1A | 9/26/09</p>
 
           <p class="port-hed"><a href="/pdfs/pension_2010_0319.pdf">Detroit pension boards targeted</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 1A | 3/19/10</p>
@@ -64,23 +48,15 @@
           <p class="port-hed"><a href="/pdfs/watson_2009_524.pdf">Councilwoman paid pittance for taxes on 'nonexistent' Detroit house</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 1A | 5/24/09</p>
 
-          <p class="port-hed"><a href="/pdfs/cockrel_2009_920.pdf">Cockrel says it's tough to be an incumbent, but he's determined to keep the presidency</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 11A | 9/20/09</p>
-          <p class="port-hed"><a href="/pdfs/hendrix_2009_0213.pdf">Hendrix works to appeal to all kinds of voters</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 1B | 2/13/09</p>   
-
           <p class="port-hed"><a href="/pdfs/deconstruct_2010_919.pdf">Deconstructing Detroit</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 8A | 9/19/10</p>
 
-          <p class="port-hed"><a href="/pdfs/curb_2010_719.pdf">Detroit plays catch-up in installing curb ramps</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 3A | 7/19/10</p>
-          
-          <p class="port-hed"><a href="/pdfs/depot_2009_1129.pdf">Once crumbling train stations gleam in 2 cities</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 8A | 11/29/09</p>
-          
           <p class="port-hed"><a href="/pdfs/gay_2010_713.pdf">Black, gay, proud</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 6A | 7/23/10</p>
          
+          <p class="port-hed"><a href="/pdfs/security_2011_0606.pdf">Palmer Woods residents on board with paramilitary-style patrols</a></p>
+          <p class="port-tag"><em>Detroit Free Press</em> | 1A | 6/6/11</p>
+          
           <p class="port-hed"><a href="/pdfs/jena_2007_920.pdf">To metro Detroiters, rally in Jena is a fight for equality</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 1A | 9/20/07</p>
           
@@ -90,12 +66,8 @@
           <p class="port-hed"><a href="/pdfs/hankjones_2007_427.pdf">OU to bestow honorary degree on legend</a></p>
           <p class="port-tag"><em>Detroit Free Press</em> | 1B | 4/27/07</p>
          
-          <p class="port-hed"><a href="/pdfs/fugitives_2008_605.pdf">1,000 fugitives line up to get right with law</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 1B | 6/5/08</p>  
-          
-          <p class="port-hed"><a href="/pdfs/zach_2010_215.pdf">Wish for golf cart is fulfilled for boy with muscular dystrophy</a></p>
-          <p class="port-tag"><em>Detroit Free Press</em> | 3A | 2/15/10</p>
-
+          <p class="port-hed"><a href="/pdfs/buenosaires_2011_116.pdf">South America, city and country</a></p>
+          <p class="port-tag"><em>Detroit Free Press</em> | 7E | 1/26/11</p>
         </div>
         
       </div>
