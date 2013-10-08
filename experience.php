534f7bfd2c7d045ec0328ded27fed6f4eb7d1d50
parisminton <parisminton@da.ydrea.ms>
2013-10-08 03:37:50 -0400

    Removed period from date line.

--- experience.php
+++ experience.php
@@ -42,51 +42,51 @@
           <h2 style="margin:50px 0px 10px 0px;">Experience</h2>
           
           <!-- <h3>Current</h3> -->
-          <p class="res-hed">Senior Director Public Relations, October 2012 - present.<br />
+          <p class="res-hed">Senior Director Public Relations, October 2012 - present<br />
           <em>Palace Sports & Entertainment</em> | Auburn Hills, Mich.</p>
           <p class="res-sum">Manage strategic communications planning and development for all concert/entertainment events and corporate communications for PS&E properties: The Palace of Auburn Hills, DTE Energy Music Theatre, Meadow Brook Music Festival and the Detroit Pistons. Prepare and implement fiscal year departmental budget.</p>
           
-          <p class="res-hed">Press Secretary, January 2012 - September 2012.<br />
+          <p class="res-hed">Press Secretary, January 2012 - September 2012<br />
           <em>Mayor Dave Bing, City of Detroit</em> | Detroit, Mich.</p>
           <p class="res-sum">Responsible for developing and managing strategic policy messaging and crisis communications; coordinating City departments' communications; media relations and speechwriting.</p>
           
-          <p class="res-hed">Staff Writer, April 2003 - December 2011.<br />
+          <p class="res-hed">Staff Writer, April 2003 - December 2011<br />
           <em>Detroit Free Press</em> | Detroit, Mich.</p>
           <p class="res-sum">Provided general assignment, enterprise and City of Detroit coverage. Also covered Detroit Police and the Wayne County Sheriff's Office, and provided western Wayne County and southeast Michigan coverage; including local government, education, business and features.</p>
           
-          <p class="res-hed">Editorial Assistant, August 2002 - March 2003.<br />
+          <p class="res-hed">Editorial Assistant, August 2002 - March 2003<br />
           <em>Wisconsin Historical Society Press</em> | Madison, Wis.</p>
           <p class="res-sum">Researched, copy edited, and proofread book projects and in-house newsletter.</p>
           
-          <p class="res-hed">Staff Writer, December 1999 - March 2003.<br />
+          <p class="res-hed">Staff Writer, December 1999 - March 2003<br />
           <em>Wisconsin State Journal</em> | Madison, Wis.</p>
           <p class="res-sum">Provided general assignment coverage; including police, city and county government in Madison and Dane County.</p>
           
-          <p class="res-hed">Shelter advocate, September 1999-September 2000.<br />
+          <p class="res-hed">Shelter advocate, September 1999-September 2000<br />
           Domestic Abuse Intervention Services (DAIS) | Madison, Wis.</p>
           <p class="res-sum">Facilitated resident intake and provide client services at a shelter for abused women and families serving Dane County, Wis. Answered 24-hour Crisis Line, provided non-emergency safety planning and protection/restraining orders instruction, and provided referrals to community resources and various DAIS programs.</p>
           
-          <p class="res-hed">Editorial Assistant, January 1999 - January 2000.<br />
+          <p class="res-hed">Editorial Assistant, January 1999 - January 2000<br />
           <em>Journal of Human Resources</em> | Madison, Wis.</p>
           <p class="res-sum">Maintained peer-review of manuscripts for quarterly, academic economic journal.</p>
           
-          <p class="res-hed">Communications Coordinator, August 1996 - January 1999.<br />
+          <p class="res-hed">Communications Coordinator, August 1996 - January 1999<br />
           Household & Institutional Products Information Council/Consumer Specialty Products Association | Washington, D.C.</p>
           <p class="res-sum">Wrote and edited briefs/media responses, and analysis of state and U.S. Environmental Protection Agency/Superfund hazardous waste regulations. Assisted strategic development and implementation of media campaigns with contract public relations firms; produced one-sheets, press releases, collateral materials. Tracked media coverage/placement.</p>
           
-          <p class="res-hed">Assistant to the Director of Publications, September 1994 - August 1995.<br />
+          <p class="res-hed">Assistant to the Director of Publications, September 1994 - August 1995<br />
           Optical Society of America | Washington, D.C.</p>
           <p class="res-sum">Produced project presentations and budget reports for publications department, which published a dozen academic journals for the international professional association. Edited website content. Managed copyright requests and registered copyrights with the Library of Congress.</p>
           
-          <p class="res-hed">Staff Writer, September 1993 - December 1993.<br />
+          <p class="res-hed">Staff Writer, September 1993 - December 1993<br />
           <em>The Onion</em> | Madison, Wis.</p>
           <p class="res-sum">Reviewed films and music for satirical weekly.</p>
           
-          <p class="res-hed">Staff Writer/Managing Editor, October 1992 - August 1993.<br />
+          <p class="res-hed">Staff Writer/Managing Editor, October 1992 - August 1993<br />
           <em>Milwaukee Community Journal</em> | Milwaukee, Wis.</p>
           <p class="res-sum">Provided city and state government, education and features coverage for African-American weekly newspaper.  Assigned stories, and managed production and editorial content for the WEEKEND edition.</p>
           
-          <p class="res-hed">Staff Writer/Editor, November 1990 - May 1992.<br />
+          <p class="res-hed">Staff Writer/Editor, November 1990 - May 1992<br />
           <em>The Daily Cardinal</em> | Madison, Wis.</p>
           <p class="res-sum">Covered city, state, national and international news for student newspaper; including arts and sports. Edited "Page 2" essay/opinion column.</p>
           
