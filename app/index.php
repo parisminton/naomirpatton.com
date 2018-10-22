cac69df3e6f4f17ff53a9befdaf14b6911817416
parisminton <parisminton@da.ydrea.ms>
2018-10-22 02:43:21 -0400

    Home page copy edits.

--- app/index.php
+++ app/index.php
@@ -40,8 +40,8 @@
           <p class="summary-first">I’m more than a writer, I’m a communicator. In digital and traditional forms, from corporate, public affairs and social sector communications to weekly newspapers and metropolitan dailies, I tell and elevate the story.</p>
           
           <img src="/photos/naomi_home_portrait3.jpg" alt="Naomi R. Patton has worked in journalism and communications for nearly 20 years." id="home-portrait" />
-          <p class="summary">I’ve developed messaging and communication campaigns, highlighting Detroit Mayor Dave Bing’s policy agenda and achievements. And managed integrated marketing campaigns as the strategic corporate and entertainment communications lead for the parent company of an NBA team.</p>
-          <p class="summary">Working extensively as counsel to public relations agency clients across philanthropy, public affairs, corporate communications, thought leadership and issues and reputation management, I’ve approached each as an opportunity invest in their people and purpose, to successfully tell their story.</p>
+          <p class="summary">I’ve developed messaging and communication campaigns, highlighting Detroit Mayor Dave Bing’s policy agenda and achievements. And I've managed integrated marketing campaigns as the strategic corporate and entertainment communications lead for the parent company of an NBA team.</p>
+          <p class="summary">Working extensively as counsel to public relations agency clients across philanthropy, public affairs, corporate communications, thought leadership and issues and reputation management, I’ve approached each as an opportunity to invest in their people and purpose, to successfully tell their story.</p>
           <p class="summary" style="margin-top: 20px;">Writer. Speechwriter. Public Relations Consultant. Content Strategist. The titles change. The platforms evolve. The storytelling is constant.</p>
         </div>
 
