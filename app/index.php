f4c661fcaac0aa7a4bf4748a0f930ff47d7aa442
parisminton <parisminton@da.ydrea.ms>
2018-10-22 02:38:01 -0400

    Update home page copy. Shrink body text size.

--- app/index.php
+++ app/index.php
@@ -37,14 +37,12 @@
       <div id="main">
       
         <div id="home-block">
-          <h2><a name="nrphome" id="home">Home</a></h2>
-          <p class="summary">I’m more than a writer. I’m a communicator. In digital and traditional forms, from corporate, public affairs and social sector communications to weekly newspapers and metropolitan dailies, I tell and elevate the story.</p>
+          <p class="summary-first">I’m more than a writer, I’m a communicator. In digital and traditional forms, from corporate, public affairs and social sector communications to weekly newspapers and metropolitan dailies, I tell and elevate the story.</p>
           
           <img src="/photos/naomi_home_portrait3.jpg" alt="Naomi R. Patton has worked in journalism and communications for nearly 20 years." id="home-portrait" />
-          <p class="summary">I’ve chronicled insurance redlining in Milwaukee's central city, and stood in Metro Airport, Christmas Day, 2009, for hours until federal agents left the scene after apprehending a would-be terrorist, who tried to blow up a commercial flight with explosives in his underwear.</p>
-          <p class="summary">I’ve helped develop national campaigns and website content for Washington, D.C.-based professional and trade associations. And helped manage integrated marketing campaigns as the strategic corporate and entertainment communications lead for the parent company of an NBA team.</p>
+          <p class="summary">I’ve developed messaging and communication campaigns, highlighting Detroit Mayor Dave Bing’s policy agenda and achievements. And managed integrated marketing campaigns as the strategic corporate and entertainment communications lead for the parent company of an NBA team.</p>
           <p class="summary">Working extensively as counsel to public relations agency clients across philanthropy, public affairs, corporate communications, thought leadership and issues and reputation management, I’ve approached each as an opportunity invest in their people and purpose, to successfully tell their story.</p>
-          <p class="summary" style="margin-top: 20px;">Writer. Spokesperson. Public Relations Consultant. Content Strategist. The titles change. The platforms evolve. The storytelling is constant.</p>
+          <p class="summary" style="margin-top: 20px;">Writer. Speechwriter. Public Relations Consultant. Content Strategist. The titles change. The platforms evolve. The storytelling is constant.</p>
         </div>
 
       </div>
